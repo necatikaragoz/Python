f53e561eed65b4b00eb4448e53c379ceb713c3bb
necati <necati_karagoz@hotmail.com>
2020-11-14 07:47:22 +0300

    comment eklendi

--- KodlamaEgzersizi/donguler/odevler/problem1.py
+++ KodlamaEgzersizi/donguler/odevler/problem1.py
@@ -1,6 +1,12 @@
 print("This program check numbers is perfect or not")
 print(" press q for exit")
 
+"""
+Kullanıcıdan aldığınız bir sayının mükemmel olup olmadığını bulmaya çalışın.
+
+Bir sayının kendi hariç bölenlerinin toplamı kendine eşitse bu sayıya "mükemmel sayı" denir. Örnek olarak, 6 mükemmel bir sayıdır. (1 + 2 + 3 = 6)
+"""
+
 while True:
 
     number = input("please insert a number")
--- KodlamaEgzersizi/donguler/odevler/problem2.py
+++ KodlamaEgzersizi/donguler/odevler/problem2.py
@@ -1,6 +1,13 @@
 print("This program check that number is a Armstrong number")
 print(" press q for exit")
+"""
+Kullanıcıdan aldığınız bir sayının "Armstrong" sayısı olup olmadığını bulmaya çalışın.
 
+Örnek olarak, Bir sayı eğer 4 basamaklı ise ve oluşturan rakamlardan herbirinin 4. kuvvetinin toplamı( 3 basamaklı sayılar için 3.kuvveti ) o sayıya eşitse bu sayıya "Armstrong" sayısı denir.
+
+Örnek olarak : 1634 = 1^4 + 6^4 + 3^4 + 4^4
+
+"""
 
 
 while True:
